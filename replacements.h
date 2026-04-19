de48a0c4b57af965bb29a09cebddb81510c44df3
kali <sharktallica@gmail.com>
2026-04-19 20:01:15 +0100

    Update replacements.h

--- replacements.h
+++ replacements.h
@@ -20,18 +20,18 @@
 struct Replacement {
     const char *match;
     const char *replacement;
-    const int *standalone;
+    const int standalone;
 };
 
 
 
 static const struct Replacement AMD_REPLACES[] = {
-    { " 486 DX", " Am486DX", 0 },
-    { "Am486DX/2", "Am486DX", 0 },
-    { "Am486DX/4", "Am486DX4/Am5x86", 0 },
-    { "K6 3D+", "K6-III", 0 },
-    { "K6 3D", "K6-2", 0 },
-    { "K6-III", "K6-2+/K6-III+", 1 }
+    { " 486 DX",        " Am486DX",         0 },
+    { "Am486DX/2",      "Am486DX",          0 },
+    { "Am486DX/4",      "Am486DX4/Am5x86",  0 },
+    { "K6 3D+",         "K6-III",           0 },
+    { "K6 3D",          "K6-2",             0 },
+    { "K6-III",         "K6-2+/K6-III+",    1 }
 };
 static const int AMD_REPLACES_LEN = sizeof(AMD_REPLACES) / sizeof(AMD_REPLACES[0]);
 
@@ -74,27 +74,27 @@ static const int DELETIONS_LEN = sizeof(DELETIONS) / sizeof(DELETIONS[0]);
 
 
 static const struct Replacement INTEL_REPLACES[] = {
-    { " 486 SX", " i486SX", 0 },
-    { "486SX/2", "486SX2", 0 },
-    { " 486 DX/4", "DX4", 0 },
-    { " 486 DX", " i486DX", 0 },
-    { "486DX-50", "486DX", 0 },
-    { "486DX/2", "486DX2", 0 },
-    { "OverDrive PODP5V63", "Pentium OverDrive", 0 },
-    { "OverDrive PODP5V83", "Pentium OverDrive", 0 },
-    { "Pentium 60/66", "Pentium (P5)", 0 },
-    { "Pentium 75 - 200", "Pentium (P54C)", 0 },
-    { "Pentium 4 - M", "Pentium 4-M", 0 },
-    { "Core2", "Core 2", 0 },
-    { "Generation Core", "Gen Core", 0 }
+    { " 486 SX",                " i486SX",              0 },
+    { "486SX/2",                "486SX2",               0 },
+    { " 486 DX/4",              "DX4",                  0 },
+    { " 486 DX",                " i486DX",              0 },
+    { "486DX-50",               "486DX",                0 },
+    { "486DX/2",                "486DX2",               0 },
+    { "OverDrive PODP5V63",     "Pentium OverDrive",    0 },
+    { "OverDrive PODP5V83",     "Pentium OverDrive",    0 },
+    { "Pentium 60/66",          "Pentium (P5)",         0 },
+    { "Pentium 75 - 200",       "Pentium (P54C)",       0 },
+    { "Pentium 4 - M",          "Pentium 4-M",          0 },
+    { "Core2",                  "Core 2",               0 },
+    { "Generation Core",        "Gen Core",             0 }
 };
 static const int INTEL_REPLACES_LEN = sizeof(INTEL_REPLACES) / sizeof(INTEL_REPLACES[0]);
 
 
 
 static const struct Replacement IDT_REPLACES[] = {
-    { " 05/04", " WinChip", 0 },
-    { "WinChip 2-3D", "WinChip 2/3", 0 }
+    { " 05/04",         " WinChip",     0 },
+    { "WinChip 2-3D",   "WinChip 2/3",  0 }
 };
 static const int IDT_REPLACES_LEN = sizeof(IDT_REPLACES) / sizeof(IDT_REPLACES[0]);
 
