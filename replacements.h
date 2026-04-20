1db694e428e9fa57435d916d51914582e0646944
kali <sharktallica@gmail.com>
2026-04-20 20:26:18 +0100

    Add overrides for Intel Core CPUs for compact mode

--- replacements.h
+++ replacements.h
@@ -39,6 +39,12 @@ static const int AMD_REPLACES_LEN = sizeof(AMD_REPLACES) / sizeof(AMD_REPLACES[0
 
 static const struct Replacement COMPACT_CPU_GPU_REPLACES[] = {
     { " Adapter",               "",     0 },
+    { "Core Solo",              "CS",   0 },
+    { "Core Duo",               "CD",   0 },
+    { "Core 2 Duo",             "C2D",  0 },
+    { "Core 2 Extreme",         "C2E",  0 },
+    { "Core 2 Quad",            "C2Q",  0 },
+    { "Core i",                 "i",    0 },
     { "Express ",               "",     0 },
     { "Extreme Graphics",       "ET",   0 },
     { " for ",                  " ",    0 },
@@ -49,7 +55,7 @@ static const struct Replacement COMPACT_CPU_GPU_REPLACES[] = {
     { "Quadro ",                "",     0 },
     { "Mobile ",                "",     0 },
     { "Radeon ",                "",     0 },
-    { "Radeon/",                "",      0 }
+    { "Radeon/",                "",     0 }
 };
 static const int COMPACT_CPU_GPU_REPLACES_LEN = sizeof(COMPACT_CPU_GPU_REPLACES) / sizeof(COMPACT_CPU_GPU_REPLACES[0]);
 
