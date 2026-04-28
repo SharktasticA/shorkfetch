4187d609ee09e334346242eeee994ed30e5a8342
kali <sharktallica@gmail.com>
2026-04-28 21:06:06 +0100

    Tweaks to GMA 9x0 device names and exclusions

--- exclusions.h
+++ exclusions.h
@@ -17,15 +17,19 @@
 #define EXCLUSIONS
 
 static const int EXCLUDED_PCI_DIDS[] = {
-    0x2973,     // 380 for Intel iGPU 2972
-    0x2983,     // 380 for Intel iGPU 2982
-    0x2993,     // 380 for Intel iGPU 2992
-    0x29a3,     // 380 for Intel iGPU 29a2
-    0x29b3,     // 380 for Intel iGPU 29b2
-    0x29c3,     // 380 for Intel iGPU 29c2
-    0x29d3,     // 380 for Intel iGPU 29d2
-    0x2a03,     // 380 for Intel iGPU 2a02
-    0x2a13      // 380 for Intel iGPU 2a12
+    0x2776,     // 380 for 8086:2772
+    0x2782,     // 380 for 8086:2582
+    0x2792,     // 380 for 8086:2592
+    0x27a6,     // 380 for 8086:27ae
+    0x2973,     // 380 for 8086:2972
+    0x2983,     // 380 for 8086:2982
+    0x2993,     // 380 for 8086:2992
+    0x29a3,     // 380 for 8086:29a2
+    0x29b3,     // 380 for 8086:29b2
+    0x29c3,     // 380 for 8086:29c2
+    0x29d3,     // 380 for 8086:29d2
+    0x2a03,     // 380 for 8086:2a02
+    0x2a13      // 380 for 8086:2a12
 };
 static const int EXCLUDED_PCI_DIDS_LEN = sizeof(EXCLUDED_PCI_DIDS) / sizeof(EXCLUDED_PCI_DIDS[0]);
 
